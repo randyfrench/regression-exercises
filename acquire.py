20db341085b2053c83aa4ea770c099df0de703a2
randyfrench <randy.a.french@gmail.com>
2021-09-07 16:57:49 -0500

    first push

--- acquire.py
+++ acquire.py
@@ -40,22 +40,70 @@ def new_zillow_data():
     
     return df
 
-def get_zillow_data():
+
+#***************************************************** Telco Data *****************************************************
+
+# Acquire Telco Data
+
+# Create function to get the necessary connection url.
+def get_connection(db, user=user, host=host, password=password):
     '''
-    This function reads in zillow data from Codeup database, writes data to
+    This function uses my info from my env file to
+    create a connection url to access the Codeup db. It takes in a string 
+    name of a database as an argument
+    '''
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+
+#create function to retrieve telco data
+def new_telco_data():
+    '''
+    This function reads the Telco data from the Codeup db
+    and returns a pandas DataFrame with three joined tables and all columns.
+    '''
+    
+    sql_query = '''
+    SELECT *
+    FROM customers
+    JOIN contract_types USING (contract_type_id)
+    JOIN payment_types USING (payment_type_id)
+    JOIN internet_service_types USING (internet_service_type_id)
+    '''
+    # Read in DataFrame from Codeup db.
+    df = pd.read_sql(sql_query, get_connection('telco_churn'))
+    
+    return df
+
+def get_telco_data():
+    '''
+    This function reads in titanic data from Codeup database, writes data to
     a csv file if a local file does not exist, and returns a df.
     '''
-    if os.path.isfile('zillow_df.csv'):
+    if os.path.isfile('telco_df.csv'):
         
         # If csv file exists read in data from csv file.
-        df = pd.read_csv('zillow_df.csv', index_col=0)
+        df = pd.read_csv('telco_df.csv', index_col=0)
         
     else:
         
         # Read fresh data from db into a DataFrame
-        df = new_zillow_data()
+        df = new_telco_data()
         
         # Cache data
-        df.to_csv('zillow_df.csv')
+        df.to_csv('telco_df.csv')
         
     return df
+
+#***************************************************** Acquire Mall Customers **************************************
+
+def get_mall_customers_data():
+    '''
+    This function reads in the mall_customers data from the Codeup db
+    and returns a pandas Dataframe
+    '''
+    
+    mall_customers_query = '''
+    SELECT *
+    FROM customers
+    '''
+    return pd.read_sql(mall_customers_query, get_connection('mall_customers'))
